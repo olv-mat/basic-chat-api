64ec89f77d837d1caaa81934ab91277bdb1c47e2
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-06-05 08:04:17 -0300

    refactor: adjust swagger settings

--- src/common/settings/swagger/swagger.properties.ts
+++ src/common/settings/swagger/swagger.properties.ts
@@ -3,3 +3,7 @@ import { ApiParam } from '@nestjs/swagger';
 export const MessageIdParm = () => {
   return ApiParam({ name: 'id', description: 'Unique message identifier' });
 };
+
+export const UserIdParam = () => {
+  return ApiParam({ name: 'id', description: 'Unique user identifier' });
+};
--- src/common/swagger/decorators/user-id-param.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { ApiParam } from '@nestjs/swagger';
-
-export const UserIdParam = () => {
-  return ApiParam({ name: 'id', description: 'Unique user identifier' });
-};
